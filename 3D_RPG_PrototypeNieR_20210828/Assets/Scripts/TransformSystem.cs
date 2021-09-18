19efa9108b3d2aca0d0a944c39d0fe78be554cec
KID <50666207+KID421@users.noreply.github.com>
2021-09-18 21:02:46 +0800

    變身與任務系統

--- 3D_RPG_PrototypeNieR_20210828/Assets/Scripts/TransformSystem.cs
+++ 3D_RPG_PrototypeNieR_20210828/Assets/Scripts/TransformSystem.cs
@@ -9,6 +9,8 @@ public class TransformSystem : MonoBehaviour
     [Header("玩家變身前後模型物件")]
     public GameObject goTransformBefore;
     public GameObject goTransformAfter;
+    [Header("攝影機")]
+    public vThirdPersonCamera cam;
     #endregion
 
     #region 欄位：私人
@@ -33,6 +35,18 @@ public class TransformSystem : MonoBehaviour
         {
             goTransformBefore.SetActive(!goTransformBefore.activeInHierarchy);
             goTransformAfter.SetActive(!goTransformAfter.activeInHierarchy);
+
+            // 攝影機 目標 設定為 目前顯示的模型
+            if (goTransformBefore.activeInHierarchy) cam.SetTarget(goTransformBefore.transform);
+            else if (goTransformAfter.activeInHierarchy) cam.SetTarget(goTransformAfter.transform);
+
+            // 同步座標
+            // goTransformBefore.transform.position = goTransformAfter.transform.position;
+            // goTransformAfter.transform.position = goTransformBefore.transform.position;
+
+
+            print("變身前座標：" + goTransformBefore.transform.position);
+            print("變身後座標：" + goTransformAfter.transform.position);
         }
     }
     #endregion
